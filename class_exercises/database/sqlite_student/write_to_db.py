ce248e4513b5bbf2c142622cd3ad17acdb5169bc
rowpri0809 <rowpri0809@highgateschool.org.uk>
2025-12-05 12:05:57 +0000

    cw

--- class_exercises/database/sqlite_student/write_to_db.py
+++ class_exercises/database/sqlite_student/write_to_db.py
@@ -1,12 +1,25 @@
 import sqlite3
-conn = sqlite3.connect('student.sqlite')
-cursor = conn.cursor()
+from faker import Faker
+import random
+
+fake = Faker('en_GB')
 
 parameterised_insert_query = """
 INSERT INTO students (firstname, lastname, age, gender)
 VALUES (?,?,?,?);
 """
 
-cursor.execute(parameterised_insert_query,('Aadit','Lamba',16,'male'))
+conn = sqlite3.connect('student.sqlite')
+cursor = conn.cursor()
+
+fake.random.seed(4321)
+random.seed(4321)
+for _ in range(10000):
+    f_name = fake.first_name()
+    l_name = fake.last_name()
+    age = random.randint(0, 114)
+    gender = random.choice(('male', 'female'))
+    cursor.execute(parameterised_insert_query, (f_name, l_name, age, gender))
+
 conn.commit()
 conn.close()
