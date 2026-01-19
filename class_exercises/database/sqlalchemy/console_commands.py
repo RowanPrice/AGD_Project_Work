d77bd32c4cfd0a69559562fe0327080d7916279b
rowpri0809 <rowpri0809@highgateschool.org.uk>
2026-01-19 15:58:58 +0000

    classwork

---
+++ class_exercises/database/sqlalchemy/console_commands.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import Session
+
+from models import Person
+
+# Connect to the activities database
+engine = create_engine('sqlite:///activities.sqlite', echo=True)
