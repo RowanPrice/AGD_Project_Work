c0f62b8821c4e9ce1ef4f5da88877deb5d6cd2ae
Home Computer <rowpri0809@highgateschool.org.uk>
2026-01-29 20:31:54 +0000

    home sqalchemy

--- class_exercises/database/sqlalchemy/CLI.py
+++ class_exercises/database/sqlalchemy/CLI.py
@@ -1,6 +1,5 @@
-# import pyinputplus as pyip
-
 from controller import Controller
+
 
 class CLI:
     def __init__(self):
@@ -10,18 +9,66 @@ class CLI:
         first_name = input('Enter first name: ')
         last_name = input('Enter last name: ')
         activities = self.controller.get_person_activities(first_name, last_name)
+
+        if not activities:
+            print("No activities found.")
+            return
+
         for activity in activities:
             print(activity)
 
     def show_all_activities(self):
-        activities = self.controller.get_all_activities()
-        for activity in activities:
+        for activity in self.controller.get_all_activities():
             print(activity)
 
     def show_all_people(self):
-        people = self.controller.get_all_people()
+        for person in self.controller.get_all_people():
+            print(person)
+
+    def show_activity_people(self):
+        activity_name = input('Enter activity name: ')
+        people = self.controller.get_activity_people(activity_name)
+
+        if not people:
+            print("No people found.")
+            return
+
         for person in people:
             print(person)
 
+    def add_person(self):
+        first_name = input("First name: ")
+        last_name = input("Last name: ")
+        self.controller.add_person(first_name, last_name)
+        print("Person added.")
+
+    def add_activity(self):
+        name = input("Activity name: ")
+        if self.controller.add_activity(name):
+            print("Activity added.")
+        else:
+            print("Activity already exists.")
+
+    def delete_person(self):
+        first_name = input("First name: ")
+        last_name = input("Last name: ")
+        if self.controller.delete_person(first_name, last_name):
+            print("Person deleted.")
+        else:
+            print("Person not found.")
+
+    def add_person_to_activity(self):
+        first_name = input("First name: ")
+        last_name = input("Last name: ")
+        activity = input("Activity name: ")
+
+        if self.controller.add_person_to_activity(first_name, last_name, activity):
+            print("Person added to activity.")
+        else:
+            print("Could not add person to activity.")
+
+
 if __name__ == '__main__':
     cli = CLI()
+
+    cli.show_all_activities()
--- class_exercises/database/sqlalchemy/controller.py
+++ class_exercises/database/sqlalchemy/controller.py
@@ -1,38 +1,99 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-
 from models import Person, Activity
 
 
 class Controller:
-    def __init__(self, db_location = 'sqlite:///activities.sqlite'):
-        self.engine = sa.create_engine(db_location)
+    def __init__(self, db_location='sqlite:///activities.sqlite'):
+        self.engine = sa.create_engine(db_location, echo=False)
+
+    def add_person(self, first_name, last_name):
+        with so.Session(self.engine) as session:
+            person = Person(first_name=first_name, last_name=last_name)
+            session.add(person)
+            session.commit()
+
+    def add_activity(self, activity_name):
+        with so.Session(self.engine) as session:
+            stmt = sa.select(Activity).where(Activity.name == activity_name)
+            if session.scalar(stmt):
+                return False
+
+            activity = Activity(name=activity_name)
+            session.add(activity)
+            session.commit()
+            return True
+
+    def delete_person(self, first_name, last_name):
+        with so.Session(self.engine) as session:
+            stmt = sa.select(Person).where(
+                Person.first_name == first_name,
+                Person.last_name == last_name
+            )
+            person = session.scalar(stmt)
+
+            if not person:
+                return False
+
+            session.delete(person)
+            session.commit()
+            return True
+
+    def add_person_to_activity(self, first_name, last_name, activity_name):
+        with so.Session(self.engine) as session:
+            person_stmt = sa.select(Person).where(
+                Person.first_name == first_name,
+                Person.last_name == last_name
+            )
+            activity_stmt = sa.select(Activity).where(Activity.name == activity_name)
+
+            person = session.scalar(person_stmt)
+            activity = session.scalar(activity_stmt)
+
+            if not person or not activity:
+                return False
+
+            if activity in person.activities:
+                return False
+
+            person.activities.append(activity)
+            session.commit()
+            return True
 
     def get_person_activities(self, first_name, last_name):
-        with so.Session(bind=self.engine) as session:
-            stmt = sa.select(Person).where(Person.first_name == first_name and Person.last_name == last_name)
-            user = session.scalar(stmt)
-            activities = user.activities
-            activity_names = [activity.name for activity in activities]
-        return activity_names
+        with so.Session(self.engine) as session:
+            stmt = sa.select(Person).where(
+                Person.first_name == first_name,
+                Person.last_name == last_name
+            )
+            person = session.scalar(stmt)
+
+            if not person:
+                return []
+
+            return [activity.name for activity in person.activities]
 
     def get_all_activities(self):
-        with so.Session(bind=self.engine) as session:
+        with so.Session(self.engine) as session:
             stmt = sa.select(Activity)
-            user = session.scalar(stmt)
-            activities = user.name
-            activity_names = [activity.name for activity in activities]
-        return activity_names
+            activities = session.scalars(stmt).all()
+            return [activity.name for activity in activities]
 
     def get_all_people(self):
-        with so.Session(bind=self.engine) as session:
+        with so.Session(self.engine) as session:
             stmt = sa.select(Person)
-            user = session.scalar(stmt)
-            people = user.first_name + ' ' + user.last_name
-            people_names = [person.name for person in people]
-        return people_names
+            people = session.scalars(stmt).all()
+            return [f"{p.first_name} {p.last_name}" for p in people]
 
+    def get_activity_people(self, activity_name):
+        with so.Session(self.engine) as session:
+            stmt = sa.select(Activity).where(Activity.name == activity_name)
+            activity = session.scalar(stmt)
 
+            if not activity:
+                return []
+
+            return [f"{p.first_name} {p.last_name}" for p in activity.attendees]
 
 if __name__ == '__main__':
     controller = Controller()
