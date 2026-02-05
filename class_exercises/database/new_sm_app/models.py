c3b427bc02bde2d7719238645084b221bc5edabf
rowpri0809 <rowpri0809@highgateschool.org.uk>
2026-02-05 10:29:47 +0000

    cw

--- class_exercises/database/new_sm_app/models.py
+++ class_exercises/database/new_sm_app/models.py
@@ -12,9 +12,9 @@ likes_table = sa.Table(
     'likes',
     Base.metadata,
     sa.Column('user_id', sa.Integer,
-              sa.ForeignKey(column='users.id',ondelete='CASCADE'),
-              primary_key=True),
-    sa.column('post_id', sa.Integer,
+              sa.ForeignKey(column='users.id', ondelete='CASCADE')
+              , primary_key=True),
+    sa.Column('post_id', sa.Integer,
               sa.ForeignKey(column='posts.id', ondelete='CASCADE'),
               primary_key=True),
 )
@@ -23,10 +23,13 @@ class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(unique=True, index=True)
-    age: Mapped[Optional[int]]
-    gender: Mapped[Optional[str]]
-    nationality: Mapped[Optional[str]]
+    age: Mapped[int]
+    gender: Mapped[str]
+    nationality: Mapped[str]
 
+    posts: Mapped[list['Post']] = relationship(
+        back_populates='user', cascade='all, delete-orphan',
+    )
 # Many-to-many: posts that are liked by the user - defined by the likes table
     liked_posts: Mapped[list['Post']] = relationship(
         secondary=likes_table,
@@ -46,17 +49,29 @@ class Post(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(unique=True, index=True)
     description: Mapped[Optional[str]]
-    user_id: Mapped[int] = mapped_column(index=True)
+    user_id: Mapped[int] = mapped_column(
+        sa.ForeignKey('users.id', ondelete='CASCADE'),
+        nullable=False,
+        index=True,
+    )
+
+    user: Mapped["User"] = relationship(
+        back_populates='posts',
+    )
 
     liked_by_users: Mapped[list['User']] = relationship(
         secondary=likes_table,
         back_populates='liked_posts',
     )
 
-    comments_made: Mapped[list['Comment']] = relationship(
+    comments: Mapped[list['Comment']] = relationship(
         back_populates='post',
         cascade = 'all, delete-orphan',
     )
+
+    @property
+    def number_of_likes(self) -> int:
+        return len(self.liked_by_users)
 
     def __repr__(self):
         return f"Post(title='{self.title}', description={self.description}, user_id='{self.user_id}')"
@@ -68,11 +83,11 @@ class Comment(Base):
         sa.ForeignKey('users.id',ondelete='CASCADE'), nullable=False
     )
     post_id: Mapped[int] = mapped_column(
-        sa.ForeignKey('post.id',ondelete='CASCADE'), nullable=False
+        sa.ForeignKey('posts.id',ondelete='CASCADE'), nullable=False
     )
     comment: Mapped[str]
     user: Mapped[User] = so.relationship(back_populates='comments_made')
-    post: Mapped[Post] = so.relationship(back_populates='comments_made')
+    post: Mapped[Post] = so.relationship(back_populates='comments')
 
     def __repr__(self):
         return f"Comment(post_id='{self.post_id}', comment={self.comment}, user_id='{self.user_id}')"
