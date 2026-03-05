3610eeaeebf14e7bc18a070250292be40a65e025
Home Computer <rowpri0809@highgateschool.org.uk>
2026-03-05 19:41:54 +0000

    pygame

--- class_exercises/pygame/game_controller.py
+++ class_exercises/pygame/game_controller.py
@@ -10,13 +10,24 @@ class Game:
         self.exit = ()
 
     def set_up(self):
-        pass
+        self.set_background_from_file("background.txt")
+        self.add_background_object('character', 'C', 'C', (1, 9))
 
-    def add_background_object(self,btype,pos):
-        if btype == 'W':
+    def add_background_object(self,type,name,btype,pos):
+        if type == "character":
+            self.characters.append(CharacterObj(name, pos, btype))
+        else:
+            self.backgrounds.append(GameObj(name, pos, btype))
 
-    def set_background_from_file(self,background_file):
-        pass
+    def set_background_from_file(self, background_file):
+        with open(background_file, "r") as file:
+            for line in file:
+                name, pos, btype = line.strip().split()
+                pos = pos.strip("()")
+                x, y = pos.split(",")
+                pos = (int(x), int(y))
+
+                self.backgrounds.append(GameObj(name, pos, btype))
 
     def check_collision(self,pos):
         pass
@@ -33,8 +44,38 @@ class Game:
     def show_game_grid(self):
         pass
 
-class GameObj(Game):
+class GameObj:
     def __init__(self,name,pos,btype):
         self.name = name
         self.pos = pos
         self.btype = btype
+
+    def __str__(self):
+        return f'Name:{self.name} Pos:{self.pos} Btype:{self.btype}'
+
+    def is_solid(self):
+        if self.btype == 'W':
+            return True
+        else:
+            return False
+
+class CharacterObj(GameObj):
+    def find_next_location(self,direction):
+        if direction == 'right':
+            return self.pos[0] + 1, self.pos[1]
+        elif direction == 'left':
+            return self.pos[0] - 1, self.pos[1]
+        elif direction == 'up':
+            return self.pos[0], self.pos[1] + 1
+        else:
+            return self.pos[0], self.pos[1] - 1
+
+    def move(self,direction):
+        if direction == 'right':
+            self.pos = (self.pos[0] + 1, self.pos[1])
+        elif direction == 'left':
+            self.pos = (self.pos[0] - 1, self.pos[1])
+        elif direction == 'up':
+            self.pos = (self.pos[0], self.pos[1] + 1)
+        else:
+            self.pos = (self.pos[0], self.pos[1] - 1)
